194b447b4956ed57472be3ee940b1d26a40bea03
Mau-Me <mauriciosm95@gmail.com>
2022-07-08 10:33:53 -0300

    🚀 Feat: Renderiza tempo recebido no relógio

--- src/components/Stopwatch/Watch/index.tsx
+++ src/components/Stopwatch/Watch/index.tsx
@@ -1,13 +1,22 @@
 import style from "./Watch.module.scss";
 
-export default function Watch() {
+interface Props {
+  tempo: number | undefined;
+}
+
+export default function Watch({ tempo = 0 }: Props) {
+  const minutos = Math.floor(tempo / 60)
+    .toString()
+    .padStart(2, "0");
+  const segundos = (tempo % 60).toString().padStart(2, "0");
+
   return (
     <>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{minutos[0]}</span>
+      <span className={style.relogioNumero}>{minutos[1]}</span>
       <span className={style.relogioDivisao}>:</span>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{segundos[0]}</span>
+      <span className={style.relogioNumero}>{segundos[0]}</span>
     </>
   );
 }
