c555d5b12b4122bbbadede6218362fc1a1b47b13
Mau-Me <mauriciosm95@gmail.com>
2022-07-08 11:14:11 -0300

    🐛 Fix: Corrigi índice passado incorretamente

--- src/components/Stopwatch/Watch/index.tsx
+++ src/components/Stopwatch/Watch/index.tsx
@@ -16,7 +16,7 @@ export default function Watch({ tempo = 0 }: Props) {
       <span className={style.relogioNumero}>{minutos[1]}</span>
       <span className={style.relogioDivisao}>:</span>
       <span className={style.relogioNumero}>{segundos[0]}</span>
-      <span className={style.relogioNumero}>{segundos[0]}</span>
+      <span className={style.relogioNumero}>{segundos[1]}</span>
     </>
   );
 }
