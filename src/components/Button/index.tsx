ed84b63262b87ee947e9bae99da35796d43ee88d
Mau-Me <mauriciosm95@gmail.com>
2022-07-08 13:38:07 -0300

    🔨 Refactor: Substitui class components por func components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,35 @@
 import React from "react";
 import style from "./Button.module.scss";
 
-class Button extends React.Component<{
+interface Props {
   children?: React.ReactNode;
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
-}> {
-  render(): React.ReactNode {
-    const { children, type = "button", onClick } = this.props;
-    return (
-      <button className={style.botao} type={type} onClick={onClick}>
-        {children}
-      </button>
-    );
-  }
 }
+
+function Button({ children, type, onClick }: Props) {
+  return (
+    <button className={style.botao} type={type} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+// CLASS COMPONENT
+
+// class Button extends React.Component<{
+//   children?: React.ReactNode;
+//   type?: "button" | "submit" | "reset" | undefined;
+//   onClick?: () => void;
+// }> {
+//   render(): React.ReactNode {
+//     const { children, type = "button", onClick } = this.props;
+//     return (
+//       <button className={style.botao} type={type} onClick={onClick}>
+//         {children}
+//       </button>
+//     );
+//   }
+// }
 
 export default Button;
